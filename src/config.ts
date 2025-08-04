eb16a32d0c28ec887e5246c03b53352a692a94ee
ahxin <ahxin1989@gmail.com>
2025-08-04 09:58:28 +0800

    update image

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
   author: "昕",
   description: "记录投资想法和记录，每周一发布，欢迎关注",
   keywords: "投资,昕",
-  icon: "https://i.111666.best/image/n70z4Qjwc2Qiaj5xnyJZTN.png",
+  icon: "/assets/logo.png",
   siteImage: "",
   homePage: "https://weekly.twocatty.xin",
   blogPage: "",
